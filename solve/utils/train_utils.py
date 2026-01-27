c729a59b325a6eac3f2982447eeff7256f566273
williammtan <william@tan.id>
2026-01-26 21:42:41 -0800

    fresh start

--- solve/utils/train_utils.py
+++
@@ -1,7 +0,0 @@
-import jax.random as jrn
-from jax import jit
-from functools import partial
-
-@partial(jit, static_argnames = ['ntr', 'batch_size'])
-def get_batch(key, ntr, batch_size): 
-    return jrn.choice(key, ntr, (batch_size,))
